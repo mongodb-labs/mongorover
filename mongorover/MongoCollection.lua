5587326fc0ed1e9f9bd2d0b44babfc73bdf329c4
yzubkov <yzubkov@gmail.com>
2016-12-05 18:01:44 -0800

    Fix return value for findIndexes()

--- mongorover/MongoCollection.lua
+++ mongorover/MongoCollection.lua
@@ -270,7 +270,7 @@ local MongoCollection = {__mode="k"}
 	-- Example usage at @{indexing.lua}.
 	-- @tparam table of fields and their corresponding index types
 	-- @tparam table of options to be used when creating index
-	-- @return string index name on success
+	-- @return string index name
 	function MongoCollection:createIndex(keys,opt)
 	    return self.collection_t:collection_create_index(luaBSONObjects, keys, opt)
 	end
@@ -278,7 +278,7 @@ local MongoCollection = {__mode="k"}
 	---
 	-- Delete an index for a specified collection.
 	-- Example usage at @{indexing.lua}.
-	-- @tparam string an index name to be deleted
+	-- @tparam string an index name or original table with indexed fields and their corresponding types
 	-- @return a boolean true if index deleted successfully
 	function MongoCollection:dropIndex(index)
 	    return self.collection_t:collection_drop_index(luaBSONObjects, index)
@@ -289,7 +289,8 @@ local MongoCollection = {__mode="k"}
 	-- Example usage at @{indexing.lua}.
 	-- @return A @{MongoCursor} with results.
 	function MongoCollection:findIndexes()
-	    return self.collection_t:collection_find_indexes(luaBSONObjects)
+		local cursor_t = self.collection_t:collection_find_indexes(luaBSONObjects)
+		return MongoCursor(self, cursor_t)
 	end
 
 
